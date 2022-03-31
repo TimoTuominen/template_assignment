7e789cdd8264f64d0f52e82287c37ce1241fb93c
Timo <timotuominen111@hotmail.com>
2022-03-31 23:32:03 +0300

    1

--- index.js
+++ index.js
@@ -8,30 +8,19 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-/*let data1 = request(
-  `https://www.omdbapi.com/?apikey=bf626253&t=matrix`,
-  function (err, res, body) {
-    if (err) {
-      return console.log(err);
-    }
-    //let data = body.json;
-    //return data;
-    //console.log(data);
-  }
-);*/
-
-/*let data;
-fetch(`https://www.omdbapi.com/?apikey=bf626253&t=matrix`)
-  .then((response) => response.json())
-  .then((json) => {
-    //console.log(json.Title);
-    //let data = json;
-    //console.log(data);
-    //console.log(json);
-    data = json;
-  }); */
-
 app.get("/", (req, res) => {
+  async function Haetiedot() {
+    let response = await fetch(
+      `https://www.omdbapi.com/?apikey=bf626253&t=matrix`
+    );
+    let data = await response.json();
+    console.log(data);
+    res.render("index", data);
+  }
+  Haetiedot();
+});
+
+app.post("/", (req, res) => {
   async function githubUsers() {
     let response = await fetch(
       `https://www.omdbapi.com/?apikey=bf626253&t=matrix`
